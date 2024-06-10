f00af31722cbcc40e9d36c1486147a21f5feecf8
Trinh Tien Minh BI12-273 <88262498+mihnnn@users.noreply.github.com>
2024-06-10 14:14:24 +0700

    404 page add

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,24 +6,25 @@ import SignUp from './components/pages/SignUp';
 import Login from './components/pages/Login';
 import Tos from './components/pages/Tos';
 import AppPage from './components/pages/AppPage';
+import PublicPage from './components/pages/PublicPage';
+import PageNotFound from './components/pages/PageNotFound'; // Import PageNotFound
 import './components/styles/App.css';
 import { useAuthContext } from './context/AuthContext';
-import PublicPage from './components/pages/PublicPage';
 
 function App() {
   const { authUser } = useAuthContext();
-  
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home /> } />
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
         <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
         <Route path="/tos" element={<Tos />} />
         <Route path="/app/*" element={authUser ? <AppPage /> : <Navigate to="/" />} />
-        {/* if authed, this route is accessable */}
-        {authUser && <Route path={`/${authUser?.username}`} element={authUser ? <PublicPage /> : <Navigate to="/" />} /> }
-        
+        {authUser && <Route path={`/${authUser.username}`} element={<PublicPage />} />}
+        {/* Catch-all route for undefined paths */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Toaster />
     </>
---
+++ front-end/src/components/pages/PageNotFound.jsx
@@ -0,0 +1,20 @@
+// src/components/pages/PageNotFound.js
+import React from 'react';
+import img_404 from '../../assets/images/img_404.jpeg';
+
+function PageNotFound() {
+  return (
+    <main className=' bg-[#222] text-emphasis flex m-auto items-center text-center flex-col'>
+      <h1 className=' text-9xl mb-10'>404 - Page Not Found</h1>
+      <p className='text-3xl'>The page you are looking for does not exist.</p>
+      {/* logo */}
+        <img
+            src={img_404}
+            alt='404'
+            className='w-96 h-96 mt-10'
+        />
+    </main>
+  );
+}
+
+export default PageNotFound;
